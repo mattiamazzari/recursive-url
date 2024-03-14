8219c2a59883389fca9be6ca7a185ac388d366c7
Mazzarimovic22 <mattiamazzari@gmail.com>
2024-03-14 19:01:53 +0100

    feat: custom parser

--- recurlsive.py
+++ recurlsive.py
@@ -1,12 +1,12 @@
 from cat.mad_hatter.decorators import hook
 
-from .recursive_url_parser import RecursiveURLExtractorParser
+from .recursive_url_parser import RecursiveUrlParser
 
 
 @hook
 def rabbithole_instantiates_parsers(file_handlers: dict, cat) -> dict:
     new_handlers = {
-        "text/html": RecursiveURLExtractorParser()
+        "text/html": RecursiveUrlParser()
     }
     file_handlers = file_handlers | new_handlers
     return file_handlers
